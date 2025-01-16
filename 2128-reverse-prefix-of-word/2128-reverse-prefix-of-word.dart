98c1eac746945776c267c8047e47bd133b41ab50
Zeenath <munavarbr@gmail.com>
2025-01-16 09:55:10 +0530

    Time: 1 ms (85.71%), Space: 149.4 MB (42.86%) - LeetHub

---
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.dart
@@ -0,0 +1,10 @@
+class Solution {
+  String reversePrefix(String word, String ch) {
+    int index=word.indexOf(ch);
+    if(index== -1){
+        return word;
+    }else{
+        return word.substring(0,index+1).split('').reversed.join('')+ word.substring(index+1);
+    }
+  }
+}
