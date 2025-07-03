33a272c0caccac8d11454e4f91a9d4b897060729
Zeenath <munavarbr@gmail.com>
2025-07-03 16:42:12 +0530

    Time: 10 ms (48.84%), Space: 146.2 MB (100%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.dart
@@ -0,0 +1,12 @@
+class Solution {
+  List<int> intersection(List<int> nums1, List<int> nums2) {
+   Set<int>set1=nums1.toSet();
+   Set<int>result={};
+   for(int num in nums2){
+    if(set1.contains(num)){
+        result.add(num);
+    }
+   } 
+   return result.toList();
+  }
+}
